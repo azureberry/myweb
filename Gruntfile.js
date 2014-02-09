b7ec523e84a42e3e90162ffc5af79cd410670438
azureberry <azureberry@>
2014-02-09 09:34:45 +0900

    grunt12

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+//'use strict';
+// livereload用の初期設定
+//var
+ //   path = require('path'),
+ //   lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;//,
+//    folderMount = function folderMount(connect, point) {
+//        return connect.static(path.resolve(point));
+//    };
+
+module.exports = function(grunt) {
+    var pkg, taskName;
+    pkg = grunt.file.readJSON('package.json');
+    grunt.initConfig({
+        // SassとCompassをコンパイルします。
+        compass: {
+            dist: {
+                options: {
+                    basePath: 'WebContent',
+                    config: 'WebContent/config.rb',
+                    load:['bower_components/foundation/scss']
+                }
+            }
+        },
+        watch: {
+            // Sassファイルが更新されたら、cmqとcsscombのタスクを実行します。
+            sass: {
+                files: ['WebContent/sass/**/*.scss','WebContent/plugin/**/*.scss'],
+                tasks: ['compass']//,
+              //  tasks: ['compass', 'cmq', 'csscomb'],
+    /*            options: {
+                    //変更されたらブラウザを更新
+                    livereload: true,
+                    nospawn: true
+                }*/
+            }
+        }//,
+        // http://localhost:9001/で表示を確認することができます。
+/*        connect: {
+            livereload: {
+                options: {
+                    port: 9001,
+                    middleware: function(connect, options) {
+                        return [lrSnippet, folderMount(connect, '.')];
+                    }
+                }
+            }
+        },*/
+        // バラバラに記述されたメディアクエリをまとめることができます。
+ /*       cmq:{
+            options: {
+                log: false
+            },
+            dev: {
+                files: {
+                    'css/': ['css/style.css']
+                }
+            }
+        },
+        // csscombでCSSプロパティを揃えます。
+        csscomb:{
+            dev:{
+                expand: true,
+                cwd: 'css/',
+                src: ['*.css'],
+                dest: 'css/'
+            }
+        }*/
+    });
+
+    // GruntFile.jsに記載されているパッケージを自動読み込み
+    for(taskName in pkg.devDependencies) {
+        if(taskName.substring(0, 6) == 'grunt-') {
+            grunt.loadNpmTasks(taskName);
+        }
+    }
+
+    //grunt.registerTask('default', ['connect', 'watch:sass']);
+    grunt.registerTask('default', ['watch:sass']);
+
+    grunt.registerTask('eatwarnings', function() {
+        grunt.warn = grunt.fail.warn = function(warning) {
+            grunt.log.error(warning);
+        };
+    });
+};
