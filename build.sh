f697d635e715161c7edd227e10227532809e2d71
azureberry <azureberry@>
2015-11-20 23:31:52 +0900

    cp

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
-# sudo npm install -g gulp
-# sudo npm install -g bower
-# # sudo npm install -g jade
-# # sudo apt install g++
+# # sudo npm install -g gulp
+# # sudo npm install -g bower
+# # # sudo npm install -g jade
+# # # sudo apt install g++
 
-cp -a /vagrant/myweb .
+# cp -a /vagrant/myweb .
 
-bundle install
-sudo npm install  (--no-bin-links)
-bower install
-node node_modules/protractor/bin/webdriver-manager update
+# bundle install
+# sudo npm install  (--no-bin-links)
+# bower install
+# node node_modules/protractor/bin/webdriver-manager update
 
-gulp build --jenkins
-gulp lint --jenkins
+ cp -a /vagrant/myweb/src/jade/fragment/ ./src/jade/
+ cp -a /vagrant/myweb/src/json/ ./src/
+ cp -a /vagrant/myweb/src/img/ ./src/
+ cp -a /vagrant/myweb/src/link/ ./src/
 
-DISPLAY=:0 gulp test --jenkins
+# gulp build --jenkins
+# gulp lint --jenkins
+
+# DISPLAY=:0 gulp test --jenkins
 
 
