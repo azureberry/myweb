02ef5bd40b8ed5b6240bdfced229ab2c90a127a8
azureberry <azureberry@>
2015-11-20 23:40:23 +0900

    cp分割

--- build.sh
+++ build.sh
@@ -1,23 +1,19 @@
-# # sudo npm install -g gulp
-# # sudo npm install -g bower
-# # # sudo npm install -g jade
-# # # sudo apt install g++
+# sudo npm install -g gulp
+# sudo npm install -g bower
+# # sudo npm install -g jade
+# # sudo apt install g++
 
-# cp -a /vagrant/myweb .
+cp -a /vagrant/myweb .
 
-# bundle install
-# sudo npm install  (--no-bin-links)
-# bower install
-# node node_modules/protractor/bin/webdriver-manager update
+bundle install
+sudo npm install  (--no-bin-links)
+bower install
+node node_modules/protractor/bin/webdriver-manager update
 
- cp -a /vagrant/myweb/src/jade/fragment/ ./src/jade/
- cp -a /vagrant/myweb/src/json/ ./src/
- cp -a /vagrant/myweb/src/img/ ./src/
- cp -a /vagrant/myweb/src/link/ ./src/
 
-# gulp build --jenkins
-# gulp lint --jenkins
+gulp build --jenkins
+gulp lint --jenkins
 
-# DISPLAY=:0 gulp test --jenkins
+DISPLAY=:0 gulp test --jenkins
 
 
---
+++ build_copy.sh
@@ -0,0 +1,4 @@
+cp -a /vagrant/myweb/src/jade/fragment/ ./src/jade/
+cp -a /vagrant/myweb/src/json/ ./src/
+cp -a /vagrant/myweb/src/img/ ./src/
+cp -a /vagrant/myweb/src/link/ ./src/
